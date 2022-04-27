372af82147f3ccec276dc925ab4eb0b0aa1079b6
OmarMohamed156 <om658036@gmail.com>
2022-04-27 15:41:40 +0200

    added graphes and adjusted logic

--- screens/Sensors.js
+++ screens/Sensors.js
@@ -1,43 +1,69 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Text, View, TextInput,Button , TouchableOpacity,Alert,ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button , ToastAndroid,Alert,ScrollView,Dimensions } from 'react-native';
 import React ,{useState} from 'react';
-
+import { VictoryChart,VictoryLine,VictoryTheme,VictoryLabel,VictoryAxis } from 'victory-native';
 const Sensors = ({ navigation,route }) => {
 
     const {patientSensors} = route.params;
+    const [sensorControl,setSensorControl] = useState(false);
+    const [turnOff,setTurnOff] = useState(true);
+    const [socketMessage,setSocketMessage]=useState('')
+    const [control,setControl] = useState(false)
+    const [showGraph,setShowGraph]=useState(false)
 
-    const[socketMessage,setSocketMessage]=useState('')
-    const [sensorID, setSensorID] = useState()
-
-    var ws = new WebSocket('ws://192.168.43.226:80/slave');
+    var ws = new WebSocket('ws://172.28.132.244:80/slave');
     ws.onmessage = msg =>{
         setSocketMessage(msg.data)
     }
 
-    // ws.OPEN ? console.log('the socket is connected'): console.log('the socket is not connected, please reconnect')
-
-
-    const getReadings = () => {
-        ws.send(JSON.stringify({type:'sensor'}))
-    }
-
-    const sensors_IDs =[]
+    const Sensors={}
 
     return(
         <SafeAreaView>
-            <ScrollView horizontal>
+                <ScrollView horizontal>
                 <View style={styles.slider}>
                 {patientSensors.map((item,index)=>{
-                    // console.log(item)
+                    Sensors[item.sensor_id] = item.sensor_id
                 return(
                     <View key={index} style={styles.capsules}>
-                        <Button  title={item.sensor_serial_number} onPress={()=>{}} />
-                        <Text>{item.sensor_id}</Text>
+                        <Button  title={item.sensor_serial_number} onPress={()=>{
+                            ws.send(JSON.stringify({type:'sensor',value:item.sensor_id}))
+                            ToastAndroid.show('Getting Sensor Data',ToastAndroid.SHORT)
+                            setShowGraph(showGraph ? false : true)
+                            setControl(control ? false : true)
+                            if(control){
+                                ws.send(JSON.stringify({type:'control',sensor_id:item.sensor_id,state:turnOff}))
+                                ToastAndroid.show('Control sent',ToastAndroid.SHORT)
+                                setTurnOff(turnOff ? false : true)
+                                setShowGraph(showGraph ? false : true)
+                            }
+                        }} />
                     </View>
                 )    
                 })}
                 </View>
             </ScrollView>
+            <View  style={{height:520,borderRadius:50}} >
+                {showGraph && 
+                    <VictoryChart width={Dimensions.get('window').width-25} theme={VictoryTheme.material} >
+                        <VictoryLine 
+                            style={{
+                                data: { stroke: '#06b6d4'}
+                              }}
+                            data={socketMessage}
+                              animate={{
+                                duration:5000,
+                                easing: 'sinOut'
+                              }}
+                        ></VictoryLine>
+                    </VictoryChart>
+                }
+                <Button  title='Turn On/Off'  onPress={()=>{
+                    ws.send(JSON.stringify({type:'control',state:sensorControl}))
+                    setSensorControl(sensorControl ? false : true)
+                    setShowGraph(showGraph ? false : true)
+                }}/>
+            </View>
         </SafeAreaView>
     )
 }
@@ -46,7 +72,7 @@ const styles = StyleSheet.create({
     slider:{
         flexDirection:'row',
         justifyContent: 'center',
-        paddingBottom:20,
+        paddingBottom:10,
     },
     capsules:{
         marginHorizontal:10,
